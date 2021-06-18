e618fa0b75186ba28a0312227bc39535785511cb
D <denise.meerkerk@gmail.com>
2021-06-18 12:21:20 +0200

    poging miljoen

---
+++ data_inspection.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  6 12:10:24 2021
+
+@author: denise
+"""
+
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import os
+import pandas as pd
+#import imageio
+#%%
+dir = os.path.dirname(__file__)
+dir_img = dir.replace('Repo', 'NLMCXR_png')
+dir = dir.replace('Repo', '')
+filename = os.path.join(dir, 'Data_with_tags.npy')
+
+#%% annotations
+data = np.load(filename, allow_pickle=True)
+datad = data.item() #dictionary
+
+#%% images
+onlyfiles = [f for f in os.listdir(dir_img) if os.path.isfile(os.path.join(dir_img, f)) and 'png' in f] #png files
+
+images = [plt.imread(dir_img + '/'+ f) for f in onlyfiles[:10]]
+#print(images.shape)
+plt.figure()
+plt.imshow(images[0])
+plt.title(onlyfiles[0])
+plt.figure()
+plt.imshow(images[1])
+plt.title(onlyfiles[1])
+
+
+
+#%% is het een veelvoorkomend probleem dat niet beide views aanwezig zijn.
+projections = pd.read_csv(os.path.join(dir,'indiana_projections.csv'))
+n_unique_patients = projections['uid'].nunique()
+projections_U=projections['uid'].unique()
+#%%voor welke ids geldt dat er zowel een lateral als een frontal is
+probleem = pd.DataFrame(projections_U,columns=['uid'])
+probleem['uid2'] = projections_U
+probleem = probleem.set_index('uid')
+# unique uid | frontal count | lateral count | both?
+probleem['Frontal']=projections.loc[projections['projection']=='Frontal']['uid'].value_counts()
+probleem['Lateral']=projections.loc[projections['projection']=='Lateral']['uid'].value_counts()
+probleem = probleem.fillna(0)
+probleem['both'] = np.where((probleem['Frontal']>= 1)&(probleem['Lateral']>= 1), True, False)
+probleem['exact'] = np.where((probleem['Frontal']== 1)&(probleem['Lateral']== 1), True, False)
+probleem['only missing L'] = np.where((probleem['Frontal']>= 1)&(probleem['Lateral']< 1), True, False)
+probleem['only missing F'] = np.where((probleem['Frontal']<1)&(probleem['Lateral']>= 1), True, False)
+
+print(probleem.head())
+
+print(probleem['only missing F'].value_counts())
+
+probleem.to_csv('probleem.csv')
+#kaggle code snippets?
+
+
---
+++ proof_of_concept.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 28 10:15:26 2021
+
+@author: denise
+"""
+import sys
+# insert at 1, 0 is the script path (or '' in REPL)
+sys.path.insert(1, '/home/denise/Documents/Vakken/Scriptie/Repo/object_relation_transformer')
+from object_relation_transformer.misc import resnet
+from object_relation_transformer.scripts import prepro_feats ####
+import numpy as np
+import pandas as pd
+
+from data_inspection import datad, images
+import tensorflow as tf
+#%% prepare images
+N = len(images)
+shape = np.shape(images[0])
+new_shape=624
+images2 = np.zeros((N,new_shape,shape[1], shape[2]))
+for n,image in enumerate(images):
+    l = np.shape(image)[0]
+    images2[n, :l, :shape[1],:shape[2]] = image
+
+#%% prepare labels normal vs not normal
+labels,index = [],[]
+for i in range(10):
+    labels.append(datad['CXR'+str(i+1)])
+    index.append('CXR'+str(i+1))
+
+info=pd.DataFrame(labels,index=index)
+info['normal?']= info.iloc[:,0]=='normal'
+
+labels = list(info["normal?"])
+    
+y = np.array(labels)
+#%% 
+'''
+resnet_model = resnet.resnet101()
+
+resnet_model.compile()
+
+resnet_model.train(images2,labels)
+'''
+
+#%% model
+model = tf.keras.applications.ResNet101(
+    include_top=True,
+    weights=None,
+    input_tensor=None,
+    input_shape=(624, 512, 3),
+    pooling=None,
+    classes=2
+)
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+history = model.fit(images2,y,validation_split=0.2, batch_size=8, epochs=2)
