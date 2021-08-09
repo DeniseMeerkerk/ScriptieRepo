f2bf9506396e2d94a7357de7ec891bbd5c1a617a
D <denise.meerkerk@gmail.com>
2021-08-09 18:48:02 +0200

    reconstruct json files of XRAY to look like coco

--- findings2json.py
+++ findings2json.py
@@ -14,9 +14,6 @@ import json
 
 dir = os.path.dirname(__file__)
 annotation_file =  dir.replace('ScriptieRepo', 'DATA/indiana_reports.csv')
-# annotations = pd.read_csv(annotation_file)
-# findings = annotations[["uid","findings"]]
-# findings["findings"] = findings["findings"].astype(str)
 
 #%% adjust json file to right directory (only test for now)
 folder = dir.replace('ScriptieRepo', 'DATA')
@@ -24,7 +21,7 @@ folder = dir.replace('ScriptieRepo', 'DATA')
 a_file = open(folder+"/test.json", "r")
 json_object = json. load(a_file)
 a_file.close()
-#print(json_object)
+#print(json_object[0])
 
 
 for n in range(len(json_object)):
@@ -37,4 +34,49 @@ for n in range(len(json_object)):
 a_file = open(folder+"/test.json", "w")
 json.dump(json_object, a_file)
 a_file.close()
+
+#%% making a subset of 10 
+subset = json_object[:10]
+new_file = open(folder+"/subset.json", "w")
+json.dump(subset, new_file)
+new_file.close()
+
+#%% inspect coco json file
+
+dir = os.path.dirname(__file__)
+annotation_file =  dir + '/data/caption_datasets/dataset_coco.json'
+coco_file = open(annotation_file)
+json_object_coco = json. load(coco_file)
+coco_file.close()
+print(json_object_coco['images'][0])
+
+#%% adjust subset to same format coco (for now only one image per report)
+
+new_IU_json = {}
+new_IU_json['dataset']= 'IU XRAY Chest 2014(?)'
+new_IU_json['images']=[]
+for image in subset:
+    tokens = image['caption'].split(' ') #->tokenize
+    tokens = [token.lower().replace('.','') for token in tokens]
+    #tokens =[token for token in tokens]
+    new_IU_json['images'].append({
+        'filepath': 'NLMCXR_png',
+        'sentids': [int(image['report_id'].split('CXR')[-1])],
+        'filename': image['images'][0].split("/",-1)[-1],
+        'imgid': image['report_id'],
+        'split': 'train',
+        'sentences': [{
+            'tokens': tokens,
+            'raw': image['caption'],
+            'imgid': int(image['report_id'].split('CXR')[-1]),
+            'sentid': int(image['report_id'].split('CXR')[-1])
+            }],
+        'report_id': int(image['report_id'].split('CXR')[-1])
+        })
+    
+#%% save adjusted jason 
+new_file = open(folder+"/subset2.json", "w")
+json.dump(new_IU_json, new_file)
+new_file.close()
+    
 
