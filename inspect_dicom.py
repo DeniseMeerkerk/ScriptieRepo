c66da2497d1f3a8d69c6949f4b39f64dc71e0ac8
D <denise.meerkerk@gmail.com>
2021-09-08 17:52:53 +0200

    inspect new dicom images with bounding box annotation

---
+++ inspect_dicom.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  8 15:40:39 2021
+
+@author: denise
+
+mostly copied from:
+https://www.kaggle.com/chandraroy/plot-dicom-data-in-python?scriptVersionId=50789952
+"""
+
+import numpy as np
+import pandas as pd
+import os
+from matplotlib import cm
+import matplotlib.pyplot as plt
+import seaborn as sns
+#import cv2
+import pydicom
+from tqdm import tqdm
+
+#%%
+TRAIN_DIR = '/home/denise/Downloads/Dicom/train'
+#TEST_DIR = '../input/vinbigdata-chest-xray-abnormalities-detection/test'
+
+df_train = pd.read_csv('/home/denise/Downloads/Dicom'+"/train.csv")
+print(df_train.shape)
+print(df_train.columns)
+df_train.head(20)
+
+print(df_train['image_id'][0])
+
+#%%
+
+def show_dcm_info(dataset):
+    print("Filename:", file_path)
+    print("Patient's Gender :", dataset.PatientSex)
+
+    
+    if 'PixelData' in dataset:
+        rows = int(dataset.Rows)
+        cols = int(dataset.Columns)
+        print("Image size : {rows:d} x {cols:d}, {size:d} bytes".format(
+            rows=rows, cols=cols, size=len(dataset.PixelData)))
+        if 'PixelSpacing' in dataset:
+            print("Pixel spacing :", dataset.PixelSpacing)
+            
+#%%
+def plot_pixel_array(dataset, figsize=(10,10)):
+    plt.figure(figsize=figsize)
+    plt.imshow(dataset.pixel_array, cmap=plt.cm.bone)
+    plt.show()
+    
+#%%
+i = 1
+num_to_plot = 5
+for file_name in os.listdir(TRAIN_DIR):
+    file_path = os.path.join(TRAIN_DIR, file_name)
+    dataset = pydicom.dcmread(file_path)
+    show_dcm_info(dataset)
+    plot_pixel_array(dataset)
+    
+    if i >= num_to_plot:
+        break
+    
+    i += 1    
