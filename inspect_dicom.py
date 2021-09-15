babf05ed53d663136da0197c97cad5496d8ce678
D <denise.meerkerk@gmail.com>
2021-09-15 12:59:49 +0200

    inspect bounding box data

--- inspect_dicom.py
+++ inspect_dicom.py
@@ -18,6 +18,7 @@ import seaborn as sns
 #import cv2
 import pydicom
 from tqdm import tqdm
+from matplotlib.patches import Rectangle
 
 #%%
 TRAIN_DIR = '/home/denise/Downloads/Dicom/train'
@@ -47,14 +48,30 @@ def show_dcm_info(dataset):
             
 #%%
 def plot_pixel_array(dataset, figsize=(10,10)):
+    image_id = file_name[:-6]
+    df_temp = df_train.loc[df_train['image_id'] == image_id]
     plt.figure(figsize=figsize)
     plt.imshow(dataset.pixel_array, cmap=plt.cm.bone)
+    for i in range( len(df_temp)):
+        if list(df_temp['class_id'])[i] == 14:
+            continue
+        else:
+            x = list(df_temp['x_min'])[i]
+            y = list(df_temp['y_min'])[i]
+            width = list(df_temp['x_max'])[i]-x
+            height = list(df_temp['y_max'])[i]-y
+            plt.gca().add_patch(Rectangle((x,y),width,height,linewidth=1,edgecolor='r',facecolor='none',label=list(df_temp['class_name'])[i]))
+            plt.text(x+10, y+50, list(df_temp['class_name'])[i],color='r')
+            plt.legend()
     plt.show()
     
 #%%
+
+
 i = 1
 num_to_plot = 5
 for file_name in os.listdir(TRAIN_DIR):
+    
     file_path = os.path.join(TRAIN_DIR, file_name)
     dataset = pydicom.dcmread(file_path)
     show_dcm_info(dataset)
